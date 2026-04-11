7c973a90bd2bd4a2202aaab87700d66fc2456816
Corbin Crutchley <git@crutchcorn.dev>
2026-04-11 01:52:58 -0700

    chore: add shared package

--- apps/backend/main.py
+++ apps/backend/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
+from shared import hello
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"message": hello()}
 
 
 @app.get("/items/{item_id}")
