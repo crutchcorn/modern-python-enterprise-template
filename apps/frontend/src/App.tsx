9eaac56cca2182385d6afd312fc067d103e4f465
Corbin Crutchley <git@crutchcorn.dev>
2026-04-12 23:54:29 -0700

    chore: link OpenAPI generation

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,9 +1,19 @@
 import { HelloWorld } from "components";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [spec, setSpec] = useState<unknown>(null);
+
+  useEffect(() => {
+    fetch("/openapi.json")
+      .then((res) => res.json())
+      .then(setSpec);
+  }, []);
+
   return (
     <>
       <HelloWorld />
+      <pre>{JSON.stringify(spec, null, 2)}</pre>
     </>
   );
 }
--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "../backend/openapi{,_spec}.json",
+          dest: ".",
+          rename: () => "../openapi.json",
+        },
+      ],
+    }),
+  ],
   root: import.meta.dirname,
 });
